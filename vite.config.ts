a116516b116cab482eb81dcaa42b6abe92b26c3f
BIRI YASSER <acil3967@gmail.com>
2025-09-23 19:16:02 +0100

    fix: update dev dependencies to correct versions

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
   },
-  base: '/admin/',
+  base: process.env.NODE_ENV === 'production' ? '/' : '/admin/',
   server: {
     port: 3003, 
     proxy: {
